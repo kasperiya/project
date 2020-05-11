14abd68c1078de6e2c3cddb0719e14ec738a268c
kasperiya <65187111+kasperiya@users.noreply.github.com>
2020-05-11 22:05:22 +0300

    asal sayı bulma

--- asal.py
+++ asal.py
@@ -1,26 +1,26 @@
 print("hoş geldiniz bir sayı giriniz asl mı degil mi onu ögreneceksiniz")
 
-sayı=int(input("sayınız nedir:"))
-bölen=[]
+sayi=int(input("sayınız nedir:"))
+bolen=[]
 sayac=0
 
 
-for i in range(2,sayı):
-    if sayı % i == 0:
+for i in range(2,sayi):
+    if sayi % i == 0:
         sayac = sayac + 1
-        bölen.append(i)
+        bolen.append(i)
 
 
 print("sayaç degeri",sayac)
-print("sayının bölenleri",bölen)
+print("sayının bölenleri",bolen)
 
 if sayac>0:
-    print(f" {sayı} sayısı asal degil")
-elif sayac==0 and sayı==1:
-    print(f" {sayı} sayısı asal degil")
-elif sayac==0 and sayı<0:
-    print(f"{sayı} sayısı negatif oldugundan asal degildir")
-elif sayac==0 and sayı==0:
-    print(f"{sayı} saysısı asal degil")
+    print(f" {sayi} sayısı asal degil")
+elif sayac==0 and sayi==1:
+    print(f" {sayi} sayısı asal degil")
+elif sayac==0 and sayi<0:
+    print(f"{sayi} sayısı negatif oldugundan asal degildir")
+elif sayac==0 and sayi==0:
+    print(f"{sayi} saysısı asal degil")
 elif sayac==0:
-    print(f"{sayı} sayısı asaldır")
+    print(f"{sayi} sayısı asaldır")
