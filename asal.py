5d25564fe5fa921de09577c16ec5e5f90b25c93b
kasperiya <65187111+kasperiya@users.noreply.github.com>
2020-05-13 22:37:59 +0300

    Update asal.py

--- asal.py
+++ asal.py
@@ -2,7 +2,7 @@ print("hoş geldiniz bir sayı giriniz asl mı degil mi onu ögreneceksiniz")
 
 number=int(input("sayınız nedir:"))
 dividing=[]
-buyc=0
+sayac=0
 
 
 for i in range(2,number):
@@ -14,13 +14,13 @@ for i in range(2,number):
 print("sayaç degeri",buyc)
 print("sayının bölenleri",dividing)
 
-if buyc>0:
+if sayac>0:
     print(f" {number} sayısı asal degil")
-elif buyc==0 and number==1:
+elif sayac==0 and number==1:
     print(f" {number} sayısı asal degil")
-elif buyc==0 and number<0:
+elif sayac==0 and number<0:
     print(f"{number} sayısı negatif oldugundan asal degildir")
-elif buyc==0 and number==0:
+elif sayac==0 and number==0:
     print(f"{number} saysısı asal degil")
-elif buyc==0:
+elif sayac==0:
     print(f"{number} sayısı asaldır")
